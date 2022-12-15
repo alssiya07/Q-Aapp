5ef0b9a469330d509ed84907a77ed8122429f837
alssiya <allukukku2000@gmail.com>
2022-12-15 11:31:09 +0530

    dec 15

--- socials/urls.py
+++ socials/urls.py
@@ -16,6 +16,23 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Q&A Socials",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 from socialapp import views
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import ObtainAuthToken
@@ -27,5 +44,7 @@ router.register("answers",views.AnswersView,basename="answers")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('token/',ObtainAuthToken.as_view())
+    path('token/',ObtainAuthToken.as_view()),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), 
+    name='schema-swagger-ui')
 ]+router.urls
